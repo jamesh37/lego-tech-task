29bd44c5bf7d10398b354e148d4d443ee0674b75
James Hodge <james.hodge37@gmail.com>
2020-06-12 08:19:02 +0100

    security groups

---
+++ modules/platform/security.tf
@@ -0,0 +1,51 @@
+# Allow port 80 ingress to load balancer, from everywhere
+resource "aws_security_group" "ext80" {
+  name        = "${var.app_name}-${var.env}-80_ext"
+  description = "Allow 80 inbound traffic"
+  vpc_id      = aws_vpc.primary.id
+
+  ingress {
+    description = "80 from everywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_80_ingress_ext"
+  }
+}
+
+#Allow port 80 ingress to containers, from load balancer
+resource "aws_security_group" "int80" {
+  name        = "${var.app_name}-${var.env}-80_int"
+  description = "Allow 80 inbound traffic from LB"
+  vpc_id      = aws_vpc.primary.id
+
+  ingress {
+    description     = "80 from everywhere"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ext80.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_80_ingress_int"
+  }
+}
---
+++ modules/platform/vars.tf
@@ -0,0 +1,27 @@
+variable "app_name" {
+
+}
+
+variable "env" {
+
+}
+
+variable "vpc_cidr" {
+
+}
+
+variable "public_cidr_1" {
+
+}
+
+variable "public_cidr_2" {
+
+}
+
+variable "private_cidr_1" {
+
+}
+
+variable "private_cidr_2" {
+
+}
